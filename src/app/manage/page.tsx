24e2e9690b32ad1c7d6030f826acc2cb070878d3
Krishna <gupta.krishnalko111@gmail.com>
2024-11-30 21:56:56 +0530

    bug fixed

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -6,30 +6,42 @@ import Title from '@/components/Title'
 import { Separator } from '@/components/ui/separator'
 import { Button } from "@/components/ui/button"
 
-import React from 'react'
+import React, { useState } from 'react'
 import ToolSet from '@/components/ToolSet'
 import { useStatus } from '@/features/appState'
 import status from '@/appwrite/status'
+import { LoaderCircle } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export default  function Manage() {
+  const router = useRouter()
   const { id, time, title, tool } = useStatus()
+  const [isSubmmited, setisSubmmited] = useState(false)
   const handleStatusEvent = ()=> {
+    setisSubmmited(true)
     status.saveDocument({id, time, title, tool})
+    setisSubmmited(false)
   }
   return (
     <div className='bg-[#010a15] -mt-8 text-white min-h-screen items-center flex flex-col justify-center'>
       <span className='text-4xl mb-8 text-orange-300 select-none hover:scale-110 transition-all'>Manage Status</span>
-      <div className='items-start flex flex-col'>
+      <div className='items-start flex flex-col px-8'>
+        <span className='select-none'>Preview</span>
       <Status id={id} time={time} title={title} tool={tool} key={id}/>
       <Separator className='w-full bg-gray-600 my-8' orientation='horizontal'/>
       <div className='flex flex-col space-y-8'>
       <Mood/>
-      <div className='flex justify-between'>
+      <div className='flex justify-between md:space-x-0 space-x-2'>
       <Title/>
       <ToolSet/>
       <Time/>
       </div>
-      <Button onClick={handleStatusEvent} className='bg-fuchsia-600 hover:bg-fuchsia-800'>Save changes</Button>
+      <Button onClick={handleStatusEvent} disabled={isSubmmited} className='bg-fuchsia-600 hover:bg-fuchsia-800'>
+        {
+          isSubmmited ? <LoaderCircle className='animate-spin'/> : "Save changes"
+        }
+      </Button>
+      <Button onClick={()=> router.push('/')}>Go Back</Button>
       </div>
       </div>
     </div>
--- src/components/Mood.tsx
+++ src/components/Mood.tsx
@@ -17,7 +17,7 @@ export default function Mood(){
   return (
     <div className='flex flex-col border-2 border-cyan-600 p-4 rounded-2xl text-white'>
         <div className='mb-4 text-orange-300 select-none hover:scale-110 transition-all cursor-pointer'>Choose icon</div>
-        <div className='flex space-x-2'>
+        <div className='flex space-x-2 flex-wrap justify-center space-y-2 items-center'>
         {
                     moodIcon && moodIcon.map(item => <Image onClick={()=> handleImage(item.id)} className='select-none hover:scale-110 transition-all cursor-pointer' src={item.image} alt='mood' height={80} width={80} key={item.id}/>)
 }
