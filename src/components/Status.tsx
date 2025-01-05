65acc79ce10f5798e38f6264605d5be787ae42f0
Krishna <gupta.krishnalko111@gmail.com>
2025-01-05 20:21:20 +0530

    added toggle feature for toolset option (quite useful sometimes)

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -127,7 +127,7 @@ export default function Status({
         ) : (
           <div className="flex flex-col text-white w-[220px] md:w-[250px] overflow-hidden whitespace-nowrap">
             <span
-              className={`text-base md:text-xl text-fuchsia-600 font-semibold w-fit select-none hover:scale-110 transition-all ${
+              className={`text-base md:text-xl text-fuchsia-600 font-semibold w-fit select-none transition-all ${
                 title.length > 25 ? "animate-scroll" : ""
               }`}
             >
